d8009108f98cebd3124126956f2aeec1b549a90e
Andrés Losada <andreslosada7@gmail.com>
2024-04-12 10:55:21 -0500

    Feat: created AdminPanel

---
+++ src/components/AdminPanel.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+type AdminPanelProps = {
+  sendStartGame: () => void;
+  sendShowForm: () => void;
+};
+const AdminPanel: React.FC<AdminPanelProps> = ({
+  sendStartGame,
+  sendShowForm,
+}) => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl text-center my-2">AdminPanel</h1>
+      <div className="flex justify-around w-1/2 mx-auto">
+        <button
+          onClick={sendStartGame}
+          className="bg-white hover:bg-gray-500 text-gray-700 font-semibold hover:text-white my-2 mx-auto py-2 px-4 border border-black hover:border-transparent rounded"
+        >
+          Start game
+        </button>
+        <button
+          onClick={sendShowForm}
+          className="bg-white hover:bg-gray-500 text-gray-700 font-semibold hover:text-white my-2 mx-auto py-2 px-4 border border-black hover:border-transparent rounded"
+        >
+          ShowForm
+        </button>
+        <button className="bg-white hover:bg-gray-500 text-gray-700 font-semibold hover:text-white my-2 mx-auto py-2 px-4 border border-black hover:border-transparent rounded">
+          Thanks
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPanel;

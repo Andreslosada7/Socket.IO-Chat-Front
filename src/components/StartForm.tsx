218e906462a25d983a5655e5750dd8331ae9321f
Andrés Losada <andreslosada7@gmail.com>
2024-04-12 10:55:47 -0500

    Bugfix: set input autofocus

--- src/components/StartForm.tsx
+++ src/components/StartForm.tsx
@@ -20,6 +20,7 @@ const StartForm: React.FC<StartFormProps> = ({ setName, setRegistered }) => {
         type="text"
         className="border-2 border-zinc-500 p-2 w-1/2 mx-auto text-black"
         autoComplete="off"
+        autoFocus
         onChange={(e) => setName(e.target.value)}
       />
       <button className="bg-white hover:bg-gray-500 text-gray-700 font-semibold hover:text-white my-2 mx-auto py-2 px-4 border border-black hover:border-transparent rounded">
